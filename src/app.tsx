f48fd08fdde0491640eae1b7dd1b7be9a782da27
devgabrielmarcondes <devgabmasantos@gmail.com>
2023-09-14 17:51:04 -0300

    feat: refactoring to integrate its api

---
+++ src/app.tsx
@@ -0,0 +1,119 @@
+import { Github, Wand2 } from "lucide-react";
+import { Button } from "./components/ui/button";
+import { Separator } from "./components/ui/separator";
+import { Textarea } from "./components/ui/textarea";
+import { Label } from "./components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
+import { Slider } from "./components/ui/slider";
+import { VideoInputForm } from "./components/video-input-form";
+
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      {/* Navbar */}
+      <div className="px-6 py-3 flex align-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com 💜 na NLW da RocketSeat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+      {/* Main */}
+      <main className="flex-1 p-6 flex gap-6">
+        {/* Textareas */}
+        <div className="flex flex-col flex-1 gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              placeholder="Inclua o prompt para a IA..."
+              className="resize-none p-4 leading-relaxed"
+            />
+            <Textarea
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+              className="resize-none p-4 leading-relaxed"
+            />
+          </div>
+
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode usar a variável{" "}
+            <code className="text-violet-400">{"{transcription}"}</code> no seu
+            prompt para adicionar o conteúdo da transcrição do vídeo
+            selecionado.
+          </p>
+        </div>
+        {/* Sidebar */}
+        <aside className="w-80 space-y-6">
+          <VideoInputForm />
+
+          <Separator />
+
+          <form className="space-y-6">
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Selecione um prompt" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="title">Título do YouTube</SelectItem>
+                  <SelectItem value="description">
+                    Descrição do YouTube
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+              <Select defaultValue="gpt3.5" disabled>
+                <SelectTrigger>
+                  <SelectValue />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você poderá customizar essa ação em breve
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+              <Slider min={0} max={1} step={0.1} />
+
+              <span className="block text-xs text-muted-foreground italic">
+                Valores mais altos tendem a deixar o resultado mais criativo e
+                com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full">
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </form>
+        </aside>
+      </main>
+    </div>
+  );
+}
